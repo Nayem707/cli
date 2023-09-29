6174a20f5fec821558b117b20eb94ca1f663007e
nayem islam <gangsn490@gmail.com>
2023-09-29 16:20:37 +0600

    change navebar & add Footer

--- src/app/components/NavbStyle.jsx
+++ src/app/components/NavbStyle.jsx
@@ -25,10 +25,7 @@ const NavbStyle = () => {
               <Link href='/products'>Products</Link>
             </li>
             <li className='p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer'>
-              <Link href='/cart'>Collection</Link>
-            </li>
-            <li className='p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer'>
-              <Link href='/contact'>Contact</Link>
+              <Link href='/cart'>Collections</Link>
             </li>
           </ul>
         </nav>
