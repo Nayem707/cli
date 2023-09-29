16ff2e1ea4e9588b146eef1bea7e763e0c370c8f
nayem islam <gangsn490@gmail.com>
2023-09-29 19:56:29 +0600

    create & move  folder navbar & others

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Home = () => {
-  return (
-    <div className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Home
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css';
 import NavbStyle from './components/NavbStyle';
-import TopNav from './components/TopNav';
+import TopNav from './components/layout/TopNav';
 import FooterStyle from './components/FooterStyle';
 
 export default function RootLayout({ children }) {
